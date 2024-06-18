d2e497d4f5904a4fc858e0c53e03cf03f88da190
Marcos Alexandre Reis Torquato dos Santos <50344569+marcos-rts@users.noreply.github.com>
2024-06-18 11:03:19 -0300

    Verificador idade

--- Desafios/src/VerificadorElegibilidadeConta.java
+++ Desafios/src/VerificadorElegibilidadeConta.java
@@ -10,6 +10,11 @@ public class VerificadorElegibilidadeConta {
         // TODO: Verificar se a idade do cliente
         // TODO: Se >= 18, imprimir "Voce esta elegivel para criar uma conta bancaria."
         // TODO: Caso contrário, imprimir "Voce nao esta elegivel para criar uma conta bancaria."
+        if (idade>=18) {
+            System.out.println("Voce esta elegivel para criar uma conta bancaria.");
+        }else if (idade<=18) {
+            System.out.println("Voce nao esta elegivel para criar uma conta bancaria.");
+        }
 
         // Fechar o scanner para evitar vazamentos de recursos
         scanner.close();
